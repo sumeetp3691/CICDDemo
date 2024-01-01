8a5d2d578e2be28b3c39777248cd600fa3035359
sumeetp3691 <106902678+sumeetp3691@users.noreply.github.com>
2024-01-01 16:04:13 +0530

    terraform file for tfsec check

---
+++ abc.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1" # Set your desired AWS region
+}
+
+resource "aws_security_group" "public_sg" {
+  name        = "public-sg"
+  description = "Public Security Group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow incoming traffic from any IP address
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH traffic from any IP address
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
